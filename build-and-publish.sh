93d78289f2cd1569fb9dc9e73dd5b3718f9011cd
Travis Frisinger <tfrisinger@8thlight.com>
2023-11-29 15:52:18 -0700

    added version

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,6 +1,7 @@
 imageName=playerfirst-staging-emitter
 repoName=playerfirst-emitter
-tagName=staging-emitter
+tagName=staging-emitter-v1
+
 echo "Building image $imageName"
 docker build -t $imageName .
 
